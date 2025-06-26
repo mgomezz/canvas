5375df7184a4beaf3460080db1ef082b4a6344a1
matiasgomez <matias.gomez@between.tech>
2025-06-26 16:23:36 +0200

    add banner gota

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Canvas from './Canvas';
+import BannerGota from './BannerGota';
 
 const App: React.FC = () => {
     return (
-        <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
-            <Canvas />
+        <div style={{ width: '100vw', height: '100vh' }}>
+            <BannerGota />
         </div>
     );
 };
---
+++ src/BannerGota.tsx
@@ -0,0 +1,8 @@
+const BannerGota: React.FC = () => {
+    return (
+        <div className="banner-gota-outside">
+            <div className="banner-gota-inside"></div>
+        </div>
+    );
+};
+export default BannerGota;
